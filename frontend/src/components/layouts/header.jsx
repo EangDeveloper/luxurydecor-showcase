31458e29ec7eb92d8051cf38ddfba32a68382b46
EangDeveloper <kimeang.horn2004@gmail.com>
2026-06-03 16:45:08 +0700

    commit-frontpage-1

--- frontend/src/components/layouts/header.jsx
+++ frontend/src/components/layouts/header.jsx
@@ -1,11 +1,31 @@
 
 import './styles/headerStyle.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faTelegram } from '@fortawesome/free-brands-svg-icons';
+
 export default function Header(){
     return(
         <>
             <div className="top-header">
-
+                <div className="location">
+                    <FontAwesomeIcon icon={faLocationDot} className="gold-icon"/><a href="#">Phnom Penh, Cambodia.</a>
+                </div>
+                <div className="paragraph">
+                    <p>Timeless Elegance. Crafted for Cambodia.</p>
+                </div>
+                <div className="social-media">
+                    <div className="number-phone">
+                        <FontAwesomeIcon icon={faPhone} className="gold-phone"/><a href="">+855 10 254 446</a>
+                    </div>
+                    <div className="facebook">
+                        <a href="https://www.facebook.com/p/Luxury-Decor-Cambodia-61583619916530/"><FontAwesomeIcon icon={faFacebookF} className='gold-social'/></a>
+                    </div>
+                    <div className="telegram">
+                        <a href="#"><FontAwesomeIcon icon={faTelegram} className='gold-social'/></a>
+                    </div>
+                </div>
             </div>
         </>
     )
--- frontend/src/components/pages/frontPage.jsx
+++ frontend/src/components/pages/frontPage.jsx
@@ -1,12 +1,31 @@
 
 import Header from '../layouts/header'
 import Footer from '../layouts/footer'
+import './styles/frontPageStyle.css'
+import logo from '../../assets/logos/logo.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 
 function frontPage(){
     return(
         <>
             <Header />
             
+            <div className="main-menu">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="search-bar">
+                    <input type="text" placeholder='Search...' />
+                </div>
+                <div className="nav">
+                    <ul className='menu-list'>
+                        <li><FontAwesomeIcon icon={faHouse}/>Home</li>
+                        <li><FontAwesomeIcon icon={faLayerGroup}/>Collections</li>
+                    </ul>
+                </div>
+            </div>
 
             <Footer />
         </>
